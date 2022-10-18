109670b61573d6accebc80d779d73e2d2585f264
Clara Lyra <ccl37@duke.edu>
2022-10-18 15:13:26 +0100

    Created Health Bar
    -health bar created with interactive slider fill
    -placeholder functionality currently can press "d" to cause "damage" and "h" to "heal", to change the amount of health
    -next steps to interact with enemies to cause damage and acquire food to boost health

--- Assets/Scenes/K Scripts/PlayerControls.cs
+++ Assets/Scenes/K Scripts/PlayerControls.cs
@@ -10,13 +10,50 @@ public class PlayerControls : MonoBehaviour {
     public Vector2 moveValue;
     public float speed;
 
+    public int curHealth;
+    public int maxHealth = 100;
+
+    public HealthBar healthBar;
+
+    void Start()
+    {
+        curHealth = maxHealth;
+        healthBar.SetMaxHealth(maxHealth);
+    }
+
     void FixedUpdate() {
         Vector3 movement = new Vector3(moveValue.x, 0.0f, moveValue.y);
 
         GetComponent<Rigidbody>().AddForce(movement * speed * Time.fixedDeltaTime); 
+
+        if( Input.GetKeyDown( KeyCode.D ) )
+        {
+            DamagePlayer(5);
+            Debug.Log("D key was pressed.");
+        }
+
+        if( Input.GetKeyDown( KeyCode.H) )
+        {
+            HealPlayer(5);
+            Debug.Log("H key was pressed.");
+        }
     }
 
     void OnMove(InputValue value) {
         moveValue = value.Get<Vector2>();
+    }
+
+    public void DamagePlayer( int damage )
+    {
+        curHealth -= damage;
+
+        healthBar.SetHealth(curHealth);
+    }
+
+    public void HealPlayer( int damage )
+    {
+        curHealth += damage;
+
+        healthBar.SetHealth(curHealth);
     }
 }
