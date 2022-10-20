bb4cc6ffe7f2396c5fcb753e7551e1b81a866b90
kmcguigan98 <72576428+kmcguigan98@users.noreply.github.com>
2022-10-20 14:56:31 +0100

    merged player movement with main scene
    Clara & kelly working on merging health bar and player movement to scene. Andy on linking enemy AI with player in main scene.

--- Assets/Scenes/C Scripts/PlayerHealthView.cs
+++ Assets/Scenes/C Scripts/PlayerHealthView.cs
@@ -1,35 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine.InputSystem;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 
-public class PlayerControls : MonoBehaviour {
-
-    public Vector2 moveValue;
-    public float speed;
+public class PlayerHealthView : MonoBehaviour
+{
 
     public int curHealth;
     public int maxHealth = 100;
 
     public HealthBar healthBar;
-
+    
     void Start()
     {
         curHealth = maxHealth;
         healthBar.SetMaxHealth(maxHealth);
     }
 
-    void FixedUpdate() {
-        Vector3 movement = new Vector3(moveValue.x, 0.0f, moveValue.y);
-
-        GetComponent<Rigidbody>().AddForce(movement * speed * Time.fixedDeltaTime); 
-
-        if( Input.GetKeyDown( KeyCode.D ) )
+    
+    void Update()
+    {
+        if( Input.GetKeyDown( KeyCode.F) )
         {
             DamagePlayer(5);
-            Debug.Log("D key was pressed.");
+            Debug.Log("F key was pressed.");
         }
 
         if( Input.GetKeyDown( KeyCode.H) )
@@ -37,10 +32,6 @@ public class PlayerControls : MonoBehaviour {
             HealPlayer(5);
             Debug.Log("H key was pressed.");
         }
-    }
-
-    void OnMove(InputValue value) {
-        moveValue = value.Get<Vector2>();
     }
 
     public void DamagePlayer( int damage )
